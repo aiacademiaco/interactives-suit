7bbbd3647220ffbdacf7261b33220a8be1d0acaa
alexanderbaquero07 <alexanderbaquero@gcflatino.org>
2023-09-27 15:32:24 -0500

    feat: default layou & global styles & interactive improvement

--- aprendelibre-interactives-suite-web/pages/courses/curso_de_ventas_edx_2a0a1382-3bdf-4325-a570-a419b1b1ef0e/section-03/interactive-01.vue
+++ aprendelibre-interactives-suite-web/pages/courses/curso_de_ventas_edx_2a0a1382-3bdf-4325-a570-a419b1b1ef0e/section-03/interactive-01.vue
@@ -1,5 +1,5 @@
 <template>
-  <h2 class="activity__title">Drag and Drop Matching Activity</h2>
+  <h1 class="activity__title">Drag and Drop Matching Activity</h1>
 
   <div class="activity-wrapper">
     <div class="draggable-options-container">
@@ -9,122 +9,164 @@
       </p>
     </div>
 
-
     <div class="match-container">
       <div class="match-option" v-for="(answer, index) in answers" :id="index" :key="index" @dragover.prevent="onDragOver"
         @drop="onDrop(answer.key)">
         <p class="match-option__text">{{ answer.label }}</p>
       </div>
     </div>
-
   </div>
+
   <div class="feedback-container" v-if="renderFeedback">
     <p class="feedback-container__text">{{ feedback.text }}</p>
   </div>
 </template>
 
 <script>
+import { ref } from 'vue'
 import confetti from "canvas-confetti";
 
 export default {
-  data() {
-    return {
-      items: [
-        { label: "Auditivos" },
-        { label: "Kineticos" },
-        { label: "Visuales" },
+  setup() {
+    // SEO
+    useHead({
+      title: 'Drag and Drop Matching Activity',
+      meta: [
+        { name: 'description', content: 'My amazing site.' }
       ],
-      answers: [
-        { key: "relajacion", label: "Hombros bajos y caídos. Mantienen la cabeza firmemente apoyada en los hombres, realizan movimientos lentos, seguros y calmados, lentos en sus respuestas motoras y verbales. Suelen tocar a otros y a sí mismos, uso de predicados verbales relacionados a sensaciones, sentimientos, materia y tacto." },
-        { key: "lenguajeCorporal", label: "Hombros levantados, parpadeo inquieto, respiración rápida y superficial, inclinación de cabeza hacia adelante, escasa expresión corporal, manos móviles, uso de predicados verbales relativos a imágenes o visión." },
-        { key: "comunicacion", label: "Hombros equilibrados, cabeza hacia atrás, tórax más desarrollado, respiración torácica, tendencia a señalarse al oído, tendencia a tocarse los labios; predicados verbales relacionados a aspectos del habla y escucha" },
-      ],
-      dragItem: null,
-      matches: {
-        Auditivos: "comunicacion",
-        Kineticos: "relajacion",
-        Visuales: "lenguajeCorporal",
+      bodyAttrs: {
+        class: 'drag-and-drop'
       },
-      renderFeedback: false,
-      feedback: {
-        correct: {
-          Visuales: "¡Así es!, los «clientes visuales» tienden a procesar la información mediante imágenes mentales. Por eso, lo más recomendable es que le presentes tu producto/servicio, de modo que lo vea antes de implementarlo o usarlo.",
-          Auditivos: "¡Correcto! Los «clientes auditivos»  suelen pensar o recordar a través de sonidos, voces e incluso, ruidos específicos. Es así, como puedes hacer uso de un discurso convincente, explicando tu producto para concretar la venta",
-          Kineticos: "¡Muy bien! Los «clientes kinéticos» construyen pensamientos como sentimientos y sensaciones físicas. Si notas que tus clientes son de este tipo, puedes generar dinámicas de interacción entre tu cliente y tu producto o servicio, de modo que pueda percibir su utilidad más allá de sus características físicas.",
-        },
-        text: "¡Inténtalo de nuevo!",
-      },
-    };
-  },
-  methods: {
-    onDragStart(item, index) {
-      this.renderFeedback = false;
-      this.feedback.text = "¡Inténtalo de nuevo!";
+    })
 
-      this.dragItem = { item, index };
-    },
-    onDragOver(event) {
+    // Reactive data
+    const feedBackAnimation = ref(false);
+
+    const items = ref([
+      { label: "Auditivos" },
+      { label: "Kineticos" },
+      { label: "Visuales" },
+    ]);
+
+    const answers = ref([
+      { key: "relajacion", label: "Hombros bajos y caídos. Mantienen la cabeza firmemente apoyada en los hombres, realizan movimientos lentos, seguros y calmados, lentos en sus respuestas motoras y verbales. Suelen tocar a otros y a sí mismos, uso de predicados verbales relacionados a sensaciones, sentimientos, materia y tacto." },
+      { key: "lenguajeCorporal", label: "Hombros levantados, parpadeo inquieto, respiración rápida y superficial, inclinación de cabeza hacia adelante, escasa expresión corporal, manos móviles, uso de predicados verbales relativos a imágenes o visión." },
+      { key: "comunicacion", label: "Hombros equilibrados, cabeza hacia atrás, tórax más desarrollado, respiración torácica, tendencia a señalarse al oído, tendencia a tocarse los labios; predicados verbales relacionados a aspectos del habla y escucha" },
+    ]);
+
+    const dragItem = ref(null);
+    const renderFeedback = ref(false);
+
+    // Data
+    const matches = {
+      Auditivos: "comunicacion",
+      Kineticos: "relajacion",
+      Visuales: "lenguajeCorporal",
+    };
+
+    const feedback = {
+      correct: {
+        Visuales: "¡Así es!, los «clientes visuales» tienden a procesar la información mediante imágenes mentales. Por eso, lo más recomendable es que le presentes tu producto/servicio, de modo que lo vea antes de implementarlo o usarlo.",
+        Auditivos: "¡Correcto! Los «clientes auditivos»  suelen pensar o recordar a través de sonidos, voces e incluso, ruidos específicos. Es así, como puedes hacer uso de un discurso convincente, explicando tu producto para concretar la venta",
+        Kineticos: "¡Muy bien! Los «clientes kinéticos» construyen pensamientos como sentimientos y sensaciones físicas. Si notas que tus clientes son de este tipo, puedes generar dinámicas de interacción entre tu cliente y tu producto o servicio, de modo que pueda percibir su utilidad más allá de sus características físicas.",
+      },
+      text: "¡Inténtalo de nuevo!",
+    };
+
+    // Methods
+    function onDragStart(item, index) {
+      renderFeedback.value = false;
+      feedback.text = "¡Inténtalo de nuevo!";
+
+      dragItem.value = { item, index };
+    }
+
+    function onDragOver(event) {
       event.preventDefault();
-    },
-    checkAnswer(selectedOption, dropedOption) {
-      return this.matches[selectedOption] === dropedOption;
-    },
-    removeMatchedItems(selectedOption, dropedOption) {
-      this.items = this.items.filter((item) => item.label !== selectedOption);
-      this.answers = this.answers.filter((answer) => answer.key !== dropedOption);
-    },
-    handleMatched(isCorrect, selectedOption, dropedOption) {
+    }
+
+    function checkAnswer(selectedOption, dropedOption) {
+      return matches[selectedOption] === dropedOption;
+    }
+
+    function removeMatchedItems(selectedOption, dropedOption) {
+      items.value = items.value.filter((item) => item.label !== selectedOption);
+      answers.value = answers.value.filter((answer) => answer.key !== dropedOption);
+    }
+
+    function handleMatched(isCorrect, selectedOption, dropedOption) {
       if (isCorrect) {
-        this.removeMatchedItems(selectedOption, dropedOption);
+        removeMatchedItems(selectedOption, dropedOption);
       }
-    },
-    setCorrectFeedback(selectedOption) {
-      this.feedback.text = this.feedback.correct[selectedOption];
-    },
-    fireConfetti() {
+    }
+
+    function setCorrectFeedback(selectedOption) {
+      feedback.text = feedback.correct[selectedOption];
+    }
+
+    function fireConfetti() {
       confetti({
         zIndex: 999,
         particleCount: 100,
         spread: 100,
       })
-    },
-    handleFeedback(isCorrect, selectedOption) {
-      if (isCorrect) {
-        this.setCorrectFeedback(selectedOption);
+    }
 
-        this.fireConfetti();
+    function handleFeedback(isCorrect, selectedOption) {
+      if (isCorrect) {
+        setCorrectFeedback(selectedOption);
+
+        fireConfetti();
       }
 
-      this.renderFeedback = true;
-    },
-    resetDragItem() {
-      this.dragItem = null;
-    },
-    onDrop(dropedOption) {
-      if (!this.dragItem) return;
-
-      const selectedOption = this.dragItem.item.label;
-      const isCorrect = this.checkAnswer(selectedOption, dropedOption);
-
-      this.handleMatched(isCorrect, selectedOption, dropedOption);
-      this.handleFeedback(isCorrect, selectedOption);
-      this.resetDragItem();
+      renderFeedback.value = true;
     }
+
+    function resetDragItem() {
+      dragItem.value = null;
+    }
+
+    function onDrop(dropedOption) {
+      if (!dragItem.value) return;
+
+      const selectedOption = dragItem.value.item.label;
+      const isCorrect = checkAnswer(selectedOption, dropedOption);
+
+      handleMatched(isCorrect, selectedOption, dropedOption);
+      handleFeedback(isCorrect, selectedOption);
+      resetDragItem();
+    }
+
+    // Return the data and methods that the component needs
+    return {
+      items,
+      answers,
+      renderFeedback,
+      feedback,
+      onDragStart,
+      onDragOver,
+      onDrop,
+      feedBackAnimation
+    };
   }
-};
+}
+
 </script>
 
 <style scoped>
 .activity__title {
   text-align: center;
+  color: #fff;
+  font-size: 28px;
 }
 
 .activity-wrapper {
-  width: 80%;
+  width: 100%;
   display: flex;
   align-items: center;
-  justify-content: space-evenly;
   margin: 6rem auto;
+  gap: 16rem;
+  justify-content: center;
 }
 
 .draggable-options-container {
@@ -136,7 +178,7 @@ export default {
 .draggable__option {
   box-sizing: border-box;
   padding: 1rem 1.5rem;
-  background: rgba(217, 217, 217, 0.58);
+  background: #eae0b7;
   border: 1px solid white;
   box-shadow: 12px 17px 51px rgba(0, 0, 0, 0.22);
   border-radius: 12px;
@@ -159,15 +201,23 @@ export default {
   transform: scale(0.95) rotateZ(1.7deg);
 }
 
+.match-container {
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+}
+
 .match-option {
   max-width: 400px;
+  border-radius: 12px;
+  border: 2px solid #D38D02;
+  padding: 1rem;
 }
 
 .match-option__text {
   font-size: 18px;
-  border: 2px solid #3F51B5;
-  border-radius: 12px;
-  padding: 1rem;
+  color: #FFF;
+  margin: 0;
 }
 
 .feedback-container {
@@ -178,5 +228,8 @@ export default {
 .feedback-container__text {
   text-align: center;
   font-size: 20px;
+  line-height: 1.3;
+  color: #fff;
+  margin: 0;
 }
 </style>

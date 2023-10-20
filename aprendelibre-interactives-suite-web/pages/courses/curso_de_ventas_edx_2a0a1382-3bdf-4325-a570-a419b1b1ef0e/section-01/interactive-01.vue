81c5b60518397dc3da8b2d6ebf2eee2a8b98dbdd
alexanderbaquero07 <alexanderbaquero@gcflatino.org>
2023-10-20 09:24:33 -0500

    feat: new grading mode

--- aprendelibre-interactives-suite-web/pages/courses/curso_de_ventas_edx_2a0a1382-3bdf-4325-a570-a419b1b1ef0e/section-01/interactive-01.vue
+++ aprendelibre-interactives-suite-web/pages/courses/curso_de_ventas_edx_2a0a1382-3bdf-4325-a570-a419b1b1ef0e/section-01/interactive-01.vue
@@ -5,15 +5,12 @@
       <h2 id="question" v-html="currentQuestion" v-if="!renderSellerDescription"></h2>
       <h2 id="seller-title" v-html="sellerType.title" v-if="renderSellerDescription"></h2>
       <div id="answer-buttons" v-show="showAnswers">
-        <button class="btn" @click="checkAnswer(true)">
-          Más como yo
-        </button>
-        <button class="btn" @click="checkAnswer(false)">
-          Menos como yo
-        </button>
+        <button class="btn" v-for="(answer, letter) in currentAnswers" :key="`answer-${letter}`"
+          @click="checkAnswer(letter)">{{
+            `${letter}. ${answer}` }}</button>
       </div>
       <p id="seller-description" v-if="renderSellerDescription" v-html="sellerType.description"></p>
-      <button id="reset-btn" @click="startQuiz()" v-if="renderRestartBtn">Reiniciar</button>
+      <button id="reset-btn" @click="resetQuiz()" v-if="renderRestartBtn">Reiniciar</button>
     </div>
   </div>
 </template>
@@ -26,45 +23,61 @@ export default {
       showAnswers: true,
       renderSellerDescription: false,
       renderRestartBtn: false,
-      questionList: [
-        "Planifico meticulosamente cada detalle y dirijo el proceso de ventas para alcanzar los resultados deseados.",
-        "Construyo relaciones sólidas y de confianza con los clientes, enfocándome en la comunicación y la empatía.",
-        "Colaboro con el cliente para llegar a una decisión consensuada, utilizando mi conocimiento para construir soluciones conjuntas.",
-        "Busco toda la información posible y controlo los aspectos críticos antes de tomar medidas en la venta.",
-        "Logro resultados y alcanzar los objetivos de venta, incluso si eso significa dirigir el proceso con poca participación del cliente.",
-        "Genero un ambiente de confianza y comprensión, asegurándome de que el cliente se sienta valorado y escuchado.",
-        "Trabajo en equipo con el cliente para encontrar soluciones que sean mutuamente beneficiosas.",
-        "Procuro tener todo bajo control, garantizando que el proceso de ventas sea efectivo y eficiente.",
-        "Dirijo el proceso y tomo decisiones rápidas y firmes para lograr resultados concretos.",
-        "Establezco relaciones cálidas y amigables con los clientes, creando un ambiente de confianza y apertura.",
-        "Colaboro activamente con el cliente, involucrándolo en la toma de decisiones y construyendo soluciones conjuntas.",
-        "Tengo toda la información y el control necesario para asegurar que el proceso de venta fluya sin problemas.",
-        "Destaco los resultados y beneficios tangibles que el cliente obtendrá al adquirir mi producto o servicio.",
-        "Resalto cómo nuestra relación se fortalecerá y cómo el cliente se beneficiará de nuestra interacción continua.",
-        "Me aseguro de que tanto el cliente como yo estemos de acuerdo en la decisión, construyendo un consenso antes de avanzar.",
-        "Garantizo que todos los aspectos y detalles se hayan abordado adecuadamente antes de finalizar el proceso de venta."
+      questionsAndAnswers: [
+        {
+          question: 'Al iniciar una venta suelo:',
+          answers: {
+            A: 'Planificar meticulosamente cada detalle y dirijo el proceso de ventas para alcanzar los resultados deseados.',
+            B: 'Construir relaciones sólidas y de confianza con los clientes, enfocándome en la comunicación y la empatía.',
+            C: 'Colaborar con el cliente para llegar a una decisión consensuada, utilizando mi conocimiento para construir soluciones conjuntas.',
+            D: 'Buscar toda la información posible y controlo los aspectos críticos antes de tomar medidas en la venta.'
+          }
+        },
+        {
+          question: 'A la hora de construir un ambiente de ventas prefiero',
+          answers: {
+            A: 'Lograr resultados y alcanzar los objetivos de venta, incluso si eso significa dirigir el proceso con poca participación del cliente.',
+            B: 'Generar un ambiente de confianza y comprensión, asegurándome de que el cliente se sienta valorado y escuchado.',
+            C: 'Trabajar en equipo con el cliente para encontrar soluciones que sean mutuamente beneficiosas.',
+            D: 'Procurar tener todo bajo control, garantizando que el proceso de ventas sea efectivo y eficiente.'
+          }
+        },
+        {
+          question: 'Durante el proceso de venta prefiero:',
+          answers: {
+            A: 'Dirigir el proceso y tomo decisiones rápidas y firmes para lograr resultados concretos.',
+            B: 'Establecer relaciones cálidas y amigables con los clientes, creando un ambiente de confianza y apertura.',
+            C: 'Colaborar activamente con el cliente, involucrándolo en la toma de decisiones y construyendo soluciones conjuntas.',
+            D: 'Tengo toda la información y el control necesario para asegurar que el proceso de venta fluya sin problemas.'
+          }
+        },
+        {
+          question: 'Cuando cierro la venta, yo...',
+          answers: {
+            A: 'Destaco los resultados y beneficios tangibles que el cliente obtendrá al adquirir mi producto o servicio.',
+            B: 'Resalto cómo nuestra relación se fortalecerá y cómo el cliente se beneficiará de nuestra interacción continua.',
+            C: 'Me aseguro de que tanto el cliente como yo estemos de acuerdo en la decisión, construyendo un consenso antes de avanzar.',
+            D: 'Garantizo que todos los aspectos y detalles se hayan abordado adecuadamente antes de finalizar el proceso de venta.'
+          }
+        }
       ],
       sellersTypes: {
         dedicated: {
-          matches: [1, 5, 9, 13],
           title: "Tu estilo de vendedor es: 'Dedicado'",
           description:
             "Una característica que se marca mucho en tu estilo de venta es la planificación, al igual que la facilidad de dirigir los procesos comerciales y el personal relacionado, asegurando el éxito en la ejecución de las tareas con el fin de obtener resultados positivos. Aunque tus clientes no necesitan participar activamente en la venta, tu experticia, conocimientos en el producto/servicio y participación te permiten solucionar sus necesidades. Esto significa que tienes una alta orientación a la tarea pero una baja orientación a las relaciones.",
         },
         related: {
-          matches: [2, 6, 10, 14],
           title: "Tu estilo de vendedor es: 'Relacionado'",
           description:
             "La clave de tu ejercicio de ventas es la “confianza”, ya que te orientas más a las relaciones que a la tarea. Esta es la manera eficaz en la que has conseguido cerrar tratos, pues eres un vendedor que apunta a mantener una comunicación abierta, valorando la opinión y necesidades de tu cliente. Te enfocas en comprender, escuchar, aceptar y empatizar, de forma general, con tus clientes, lo cual les da seguridad a la hora de adquirir tu producto o servicio, pues se sienten valorados durante el proceso de compra.",
         },
         integrated: {
-          matches: [3, 7, 11, 15],
           title: "Tu estilo de vendedor es: 'Integrado'",
           description:
             "Eres altamente orientado a las relaciones y a la tarea, pues integras perfectamente tu interés por tus clientes, construyendo una relación de confianza y colaboración con ellos, al igual que empleas tus conocimientos y experticia en tu producto o servicio para llegar a un consenso con el comprado",
         },
         separate: {
-          matches: [4, 8, 12, 16],
           title: "Tu estilo de vendedor es: 'Separado'",
           description:
             "Por la naturaleza de tu producto y de la dinámica de venta, sueles mantener una relación bastante distanciada o nula con tu cliente. Asimismo, como tienes una baja orientación tanto a la tarea como a las relaciones, eres un vendedor que requiere de todas las medidas y control total sobre la información y manejo de los recursos que se te otorgan para concretar tus ventas",
@@ -75,39 +88,42 @@ export default {
         description: ''
       },
       listOfResponses: [],
-      currentQuestion: '',
-      questionNo: 0,
       currentQuestionIndex: 0,
     }
   },
-  methods: {
-    showQuestion() {
-      const question = this.questionList[this.currentQuestionIndex];
-      this.questionNo = this.currentQuestionIndex + 1;
-
-      this.currentQuestion = this.questionNo + ". " + question;
+  computed: {
+    currentQuestion: function () {
+      return this.currentQuestionIndex + 1 + ". " + this.questionsAndAnswers[this.currentQuestionIndex].question;
     },
-    startQuiz() {
+    currentAnswers: function () {
+      return this.questionsAndAnswers[this.currentQuestionIndex].answers;
+    }
+  },
+  methods: {
+    resetQuiz() {
       this.listOfResponses = [];
       this.currentQuestionIndex = 0;
-
-      this.showQuestion();
 
       this.showAnswers = true
       this.renderRestartBtn = false
       this.renderSellerDescription = false
     },
     getTypeOfSeller() {
-      for (const key in this.sellersTypes) {
-        const sellerType = this.sellersTypes[key]
+      const countA = this.listOfResponses.filter((item) => item === "A").length;
+      const countB = this.listOfResponses.filter((item) => item === "B").length;
+      const countC = this.listOfResponses.filter((item) => item === "C").length;
+      const countD = this.listOfResponses.filter((item) => item === "D").length;
 
-        const matches = this.listOfResponses.filter((item) => {
-          return sellerType.matches.includes(item);
-        });
-
-        if (matches.length === 4) {
-          return sellerType;
-        }
+      if (countA >= 3) {
+        return this.sellersTypes.dedicated
+      } else if (countB >= 3) {
+        return this.sellersTypes.related
+      } else if (countC >= 3) {
+        return this.sellersTypes.integrated
+      } else if (countD >= 3) {
+        return this.sellersTypes.separate
+      } else {
+        return null
       }
     },
     renderTypeOfSeller() {
@@ -121,29 +137,37 @@ export default {
 
         this.renderRestartBtn = true
         this.renderSellerDescription = true
-      } else {
-        this.currentQuestionIndex++;
-        this.showQuestion();
       }
     },
     nextQuestion() {
-      if (this.listOfResponses.length >= 4) {
-        this.renderTypeOfSeller()
-      } else if (this.currentQuestionIndex < this.questionList.length) {
-        this.currentQuestionIndex++;
-        this.showQuestion();
-      }
+      this.currentQuestionIndex++;
     },
     checkAnswer(answer) {
-      if (answer) {
-        this.listOfResponses.push(this.questionNo)
-      }
+      if (this.listOfResponses.length >= 3) {
+        if (this.listOfResponses.length < 4) {
+          this.listOfResponses.push(answer)
+          const typeOfSeller = this.getTypeOfSeller()
 
-      this.nextQuestion();
+          if (typeOfSeller) {
+            this.renderTypeOfSeller()
+          } else {
+            if (this.listOfResponses.length === 3) {
+              this.nextQuestion()
+            } else {
+              console.log('No hay match para estas respuestas')
+            }
+          }
+        } else {
+          this.renderTypeOfSeller()
+        }
+      } else {
+        this.listOfResponses.push(answer)
+        this.nextQuestion()
+      }
     }
   },
   mounted() {
-    this.startQuiz();
+    this.resetQuiz();
   },
 };
 </script>
@@ -188,7 +212,7 @@ export default {
 
 .quiz #seller-description {
   font-size: 18px;
-  color: #040506;
+  color: #ffffff;
   margin: 2rem 0;
 }
 
